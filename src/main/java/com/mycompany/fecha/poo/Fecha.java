3e568f1d43b90071b4b43c17828163b64b48221d
Darly Diaz <darlydiaz04@gmail.com>
2024-04-21 18:55:19 -0500

    Código de validación de fecha y cambio de mes de numero a letra

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -1,5 +1,66 @@
 package com.mycompany.fecha.poo;
 
+import java.util.HashMap;
+
 public class Fecha {
-    // TODO: Aquí va el código de la clase fechas
+    
+    int dia;
+    int mes;
+    int anio;
+    HashMap <Integer, String> mesNuevo = new HashMap<>();
+
+
+    
+    public Fecha(int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+    
+    String fechaCorta(){
+    return (dia+"/"+mes+"/"+anio);
+    }
+    
+    Boolean validarFecha(){
+        if (dia<1 || dia>31) {
+            return false;
+        }else if (mes<1 || mes>12) {
+            return false;
+        }else if(mes == 2){
+            if (anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0) {
+                if (dia>29) {
+                  return false; 
+                }
+            }else{
+                if (dia>28) {
+                  return false; 
+                }
+            }
+        }
+        return true;
+    }
+    
+    String mesLetra(){
+        mesNuevo.put(1, "Enero");
+        mesNuevo.put(2, "Febrero");
+        mesNuevo.put(3, "Marzo");
+        mesNuevo.put(4, "Abril");
+        mesNuevo.put(5, "Mayo");
+        mesNuevo.put(6, "Junio");
+        mesNuevo.put(7, "Julio");
+        mesNuevo.put(8, "Agosto");
+        mesNuevo.put(9, "Septiembre");
+        mesNuevo.put(10, "Octubre");
+        mesNuevo.put(11, "Noviembre");
+        mesNuevo.put(12, "Diciembre");
+        
+
+        return mesNuevo.get(mes);
+    }
+    
+    String fechaLarga(){
+    
+    return (dia+" de "+mesNuevo.get(mes)+" de "+anio);
+    }
+   
 }
--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -1,5 +1,30 @@
 package com.mycompany.fecha.poo;
 
+import java.util.Scanner;
+
 public class Principal {
-    // TODO: Aquí va el código de la clase Principal
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int dia=sc.nextInt();
+        int mes=sc.nextInt();
+        int anio=sc.nextInt();
+        
+        Fecha fechaNueva = new Fecha(dia, mes, anio);
+        fechaNueva.fechaCorta();
+        fechaNueva.validarFecha();
+        fechaNueva.mesLetra();
+        fechaNueva.fechaLarga();
+        
+        if (fechaNueva.validarFecha()==true) {
+            System.out.println("La fecha es valida");
+            System.out.println(fechaNueva.fechaLarga());
+        }else{
+            System.out.println("La fecha es invalida");
+        }
+        
+
+    }
 }
